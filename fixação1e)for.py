fdccb7ab7109c96f8b45fbb6c5afcb28edc1a368
Pedroif2024 <163007428+Pedroif2024@users.noreply.github.com>
2024-03-14 16:56:15 -0300

    Add files via upload

---
+++ fixação1e)for.py
@@ -0,0 +1,6 @@
+# imprimir numeros de impares de 0 a 20 com for
+for i in range(1,21):
+    if i%2!=0:
+        print(i)
+print("fim de progama")
+    
---
+++ fixação1e)while.py
@@ -0,0 +1,8 @@
+# imprimir numeros de impares de 0 a 20
+n=0
+while n<=20:
+    if n%2!=0:
+     print(n)
+    n+=1
+print("fim de progama")
+    
