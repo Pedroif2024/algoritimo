28e27c5c266b8a1ae018acbf29dafadd3420a893
Pedroif2024 <163007428+Pedroif2024@users.noreply.github.com>
2024-03-21 11:48:47 -0300

    Add files via upload

---
+++ fixação 1 p) com for.py
@@ -0,0 +1,8 @@
+
+soma=0
+for i in range(50,71):
+    soma+=i
+
+media=soma/20
+print("A soma do svalores de 50 a 70 é:",soma)
+print("A media aritimatica dos valores de 50 a 70 é:",media)
---
+++ fixação 1p) while.py
@@ -0,0 +1,9 @@
+
+cont=50
+soma=0
+while cont<=70:
+    soma+=cont
+    cont+=1
+media=soma/20
+print("A soma do svalores de 50 a 70 é:",soma)
+print("A media aritimatica dos valores de 50 a 70 é:",media)
